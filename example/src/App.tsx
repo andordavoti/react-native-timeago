c534a6e016041633b7ecbbbeadcedc395b45ab4d
Andor Davoti <andor.davoti@gmail.com>
2020-09-29 17:32:52 +0200

    updated example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,18 +2,29 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import RNTimeago from 'rn-timeago';
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <RNTimeago />
+      <RNTimeago
+        style={styles.text}
+        dateTo={new Date()}
+        dateFrom={new Date()}
+      />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'purple',
+    fontSize: 30,
+    fontWeight: 'bold',
   },
 });
