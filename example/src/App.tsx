a64b90f7b1dd0a8c8d4d004c8f08e9f1b13e3cd2
Andor Davoti <andor.davoti@gmail.com>
2020-09-29 17:57:24 +0200

    updated example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,9 +3,21 @@ import { StyleSheet, View } from 'react-native';
 import RNTimeago from 'rn-timeago';
 
 const App: React.FC = () => {
+  console.log(new Date(1536484369695));
   return (
     <View style={styles.container}>
       <RNTimeago style={styles.text} dateTo={new Date(1536484369695)} />
+      <RNTimeago
+        style={styles.text}
+        dateFrom={new Date(2007, 0, 27)}
+        dateTo={new Date(2020, 0, 27)}
+      />
+      <RNTimeago
+        hideAgo
+        style={styles.text}
+        dateFrom={new Date(2007, 0, 27)}
+        dateTo={new Date(2020, 0, 27)}
+      />
     </View>
   );
 };
